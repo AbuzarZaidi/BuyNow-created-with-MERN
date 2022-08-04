72d56096c9c855eb31a20d196e5aaccaf6df436e
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-08-04 23:16:23 +0500

    working in category

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,29 +9,111 @@ const BottomNavbar = () => {
     <div className="container-fluid bg-dark mb-30">
         <div className="row px-xl-5">
             <div className="col-lg-3 d-none d-lg-block">
-                <Link to='/' className="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse"  style={{height: '65px', padding: '0 30px'}}>
+                <a href='#navbar-vertical' className="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse"  style={{height: '65px', padding: '0 30px'}}>
                     <h6 className="text-dark m-0"><i className="fa fa-bars mr-2"></i>Categories</h6>
                     <i className="fa fa-angle-down text-dark"></i>
-                </Link>
+                </a>
                 <nav className="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style={{width: 'calc(100% - 30px)', zIndex: '999'}}>
                     <div className="navbar-nav w-100">
                         <div className="nav-item dropdown dropright">
-                            <Link to="/" className="nav-link dropdown-toggle" data-toggle="dropdown">Dresses <i className="fa fa-angle-right float-right mt-1"></i></Link>
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Groceries & Pets <i className="fa fa-angle-right float-right mt-1"></i></div>
                             <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
-                                <Link to='/' className="dropdown-item">Men's Dresses</Link>
-                                <Link to='/' className="dropdown-item">Women's Dresses</Link>
-                                <Link to='/' className="dropdown-item">Baby's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
                             </div>
                         </div>
-                        <Link to='/'  className="nav-item nav-link">Shirts</Link>
-                        <Link to='/'  className="nav-item nav-link">Jeans</Link>
-                        <Link to='/'  className="nav-item nav-link">Swimwear</Link>
-                        <Link to='/'  className="nav-item nav-link">Sleepwear</Link>
-                        <Link to='/'  className="nav-item nav-link">Sportswear</Link>
-                        <Link to='/'  className="nav-item nav-link">Jumpsuits</Link>
-                        <Link to='/'  className="nav-item nav-link">Blazers</Link>
-                        <Link to='/'  className="nav-item nav-link">Jackets</Link>
-                        <Link to='/'  className="nav-item nav-link">Shoes</Link>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Health & Beauty <i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Men's Fashion <i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Women's Fashion<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Babies & Toys<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Home & Lifestyle<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Electronic Devices<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Electronic Accessories<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">TV & Home Appliances<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Sports & Outdoor<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Watches, Bags & Jewellery<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        <div className="nav-item dropdown dropright">
+                            <div  className="nav-link dropdown-toggle" data-toggle="dropdown">Automotive & Motorbike<i className="fa fa-angle-right float-right mt-1"></i></div>
+                            <div className="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <Link to='/shop' className="dropdown-item">Men's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Women's Dresses</Link>
+                                <Link to='/shop' className="dropdown-item">Baby's Dresses</Link>
+                            </div>
+                        </div>
+                        
+                       
                     </div>
                 </nav>
             </div>
